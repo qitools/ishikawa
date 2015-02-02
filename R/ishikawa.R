f1d1407ee471f1d57763e836c4ea536a744858ae
badgettrg <rbadgett@kumc.edu>
2015-02-02 14:52:03 -0600

    Support for topic in title

--- R/ishikawa.R
+++ R/ishikawa.R
@@ -1,5 +1,5 @@
 ishikawa <-
-function(content,theme) {
+function(content,topic,theme) {
 KUBlue = "#0022B4"
 #SkyBlue = "#6DC6E7"
 temp <- gsub('\n', '', fixed = TRUE, content, perl = TRUE)
@@ -9,7 +9,7 @@ temp <- paste('list(',temp,')')
 causes<-eval(parse(file = "", n = NULL, text = temp))
 par(col.axis="black" ,col.lab=KUBlue ,col.main=KUBlue ,col.sub=KUBlue,bg="#FFFFFF",xaxt="s",yaxt="s",mai=c(.956,.769,.769,.394),pin=c(14,14))
 cause.and.effect(cause=causes,title="",effect="Nonconformity",cex = c(1, 1, 0.9), font = c(2, 3, 2))
-mtext("Cause and effect diagram",        side=3,line=1,col=KUBlue,font=2, cex=1.3)
+mtext(topic,        side=3,line=1,col=KUBlue,font=2, cex=1.3)
 mtext("(Ishikawa or fish-bone diagram)", side=3,line=0,col=KUBlue,font=2, cex=1)
 if(theme=="KU"){display_logo(x=1.2,y=0.05)}
 }
